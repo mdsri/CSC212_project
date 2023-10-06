583e0c9da2d08bc464e03215d8a312bfe44cbd3c
a770P <a770P@DMAQU>
2023-10-06 14:15:00 +0300

    Merge branch 'main' of https://github.com/mdsri/CSC212_project.git into main

--- Project212/src/Contact.java
+++ Project212/src/Contact.java
@@ -1,4 +1,20 @@
 
 public class Contact {
 
+	String name;
+	int Phnumber;
+	String email;
+	String address;
+	String birthday;
+	String notes;
+
+	public Contact(String name, int Phnumber, String email, String address, String birthday, String notes) {
+		this.name = name;
+		this.Phnumber = Phnumber;
+		this.email = email;
+		this.address = address;
+		this.birthday = birthday;
+		this.notes = notes;
+	}
+
 }
--- Project212/src/Node.java
+++ Project212/src/Node.java
@@ -1,5 +1,6 @@
 
 public class Node<T>{
+	
 	public T data;
 	public Node<T> next;
 	
