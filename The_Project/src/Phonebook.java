2943167342cb89a3598661b3846d52d040d42af1
ACE <ACE@LAPTOP-JEI9NGNP>
2023-10-08 17:43:01 +0300

    Test Push And some comments in Phonebook class

--- The_Project/src/Phonebook.java
+++ The_Project/src/Phonebook.java
@@ -1,4 +1,8 @@
 
 public class Phonebook {
-
+//In this Class ياشباب اعتقد we need to create 2 linked list 1 from Contact class And the other one from Event Class 
+	  private LinkedList_ADT<Contact> Contacts;
+	  
+	  
+	  // event class is not ready yet
 }
